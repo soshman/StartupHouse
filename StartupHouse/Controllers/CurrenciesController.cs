f9d889a762410a1169ae1bad63884ce8f3a8a86f
Szymon Socha <soshman@gmail.com>
2020-03-05 23:34:55 +0100

    Added Currency controlled method stubs

---
+++ StartupHouse/Controllers/CurrenciesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StartupHouse.API.ApiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartupHouse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrenciesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<CurrencyApiModel> GetCurrencies ()
+        {
+            return null;
+        }
+
+        [Route("{code}")]
+        [HttpGet]
+        public CurrencyDetailsApiModel GetCurrencyData (string code, DateTime? fromDate, DateTime? toDate)
+        {
+            return null;
+        }
+    }
+}
